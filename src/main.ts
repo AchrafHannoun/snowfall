747c311d4142223eecedf84b2a4db4f1074cff2c
AchrafHann <achraf.hann@gmail.com>
2023-12-24 13:12:42 -0500

    add github link

--- src/main.ts
+++ src/main.ts
@@ -1,41 +1,37 @@
-import startMainAnimation from './functions/startMainAnimation';
+import startMainAnimation from "./functions/startMainAnimation";
 
-import './style.css'
+import "./style.css";
 
-const app = document.getElementById('app');
-const button = document?.getElementById('button') as HTMLElement
-
+const app = document.getElementById("app");
+const button = document?.getElementById("button") as HTMLElement;
 
 const fullAnimation = () => {
-    const introSection = document.getElementById('intro-section') as HTMLElement 
-    introSection.style.display='none'
-    const firstScreen = document.createElement('div');
-    firstScreen.style.background='#c5c5c5'
-    firstScreen.style.width='98%'
-    firstScreen.style.height='98%'
-    firstScreen.style.animation='first 2s'
-    app?.appendChild(firstScreen)
-    setTimeout(()=>{
-        firstScreen.style.display='none'
-    }, 3000)
-    
+  const introSection = document.getElementById("intro-section") as HTMLElement;
+  introSection.style.display = "none";
+  const firstScreen = document.createElement("div");
+  firstScreen.style.background = "#c5c5c5";
+  firstScreen.style.width = "98%";
+  firstScreen.style.height = "98%";
+  firstScreen.style.animation = "first 2s";
+  app?.appendChild(firstScreen);
+  setTimeout(() => {
+    firstScreen.style.display = "none";
+  }, 3000);
 
+  setTimeout(() => {
+    const secondScreen = document.createElement("div");
+    secondScreen.style.background = "#fed8a3";
+    secondScreen.style.width = "98%";
+    secondScreen.style.height = "98%";
+    secondScreen.style.animation = "first 2s";
+    secondScreen.style.position = "absolute";
+
+    app?.appendChild(secondScreen);
     setTimeout(() => {
-        const secondScreen = document.createElement('div')
-        secondScreen.style.background='#fed8a3'
-        secondScreen.style.width='98%'
-        secondScreen.style.height='98%'
-        secondScreen.style.animation='first 2s'
-        secondScreen.style.position='absolute'
-    
-        app?.appendChild(secondScreen)
-        setTimeout(()=>{
-            secondScreen.style.display='none'
-        }, 2000)
-    }, 1000)
+      secondScreen.style.display = "none";
+    }, 2000);
+  }, 1000);
 
-    setTimeout(startMainAnimation, 3000)
-
-  }
-button?.addEventListener('click', fullAnimation);
-
+  setTimeout(startMainAnimation, 3000);
+};
+button?.addEventListener("click", fullAnimation);
